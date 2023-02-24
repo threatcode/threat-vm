7afd52c03e2c9b77424348cefe75dab16b1679cd
Arnaud Rebillout <arnaudr@kali.org>
2023-02-24 10:13:02 +0700

    generate-mf: Add option to use sha256
    We still default to sha1, because there's no strong reason to switch to
    sha256 at the moment, and if we do, we might break old versions of
    VirtualBox of VMware that would not support it (although I have no idea
    which versions would break).
    
    For the background:
    - the version 1.x of the OVF spec only mentions SHA1 as being supported
      in the manifest (cf. [1], check chapter 5 « OVF Packages »)
    - the version 2.x of the OVF spec mentions both SHA1 and SHA256 (cf.
      [2])
    
    But who cares about the spec anyway, at least not VMware Workstation 16:
    when exporting to OVA, it gives you OVF v1 with SHA256 checksums.
    VirtualBox 7 is a bit more behaved, it allows exporting to OVF v1, which
    gives SHA1 checksums, and to OVF v2, which gives SHA256 checksums.
    
    In any case, it seems that importing a OVF v1 with SHA256 checksums
    works fine, for both VirtualBox and VMware (tested with versions given
    above). So switching to SHA256 sums would not break recent versions of
    those, but who knows, it might break older versions that didn't expect
    SHA256 in the .mf file.
    
    [1] https://www.dmtf.org/sites/default/files/standards/documents/DSP0243_1.1.1.pdf
    [2] https://www.dmtf.org/sites/default/files/standards/documents/DSP0243_2.0.0.pdf

--- scripts/generate-mf.sh
+++ scripts/generate-mf.sh
@@ -2,15 +2,30 @@
 
 set -eu
 
+sha=sha1
+SHA=SHA1
+
 fail() { echo "$@" >&2; exit 1; }
-usage() { fail "Usage: $(basename $0) OVF VMDK"; }
+usage() { fail "Usage: $(basename $0) [-s sha1|sha256] OVF VMDK"; }
 
 # Validate arguments
 
+while [ $# -gt 0 ]; do
+    case $1 in
+        -s)
+            shift
+            sha=$1
+            SHA=$(echo $1 | tr '[:lower:]' '[:upper:]')
+            ;;
+        *) break ;;
+    esac
+    shift
+done
+
 [ $# -eq 2 ] || usage
 
-ovf=$1; shift
-vmdk=$1; shift
+ovf=$1
+vmdk=$2
 
 [ ${ovf##*.} = ovf ] || fail "Invalid input file '$ovf'"
 [ ${vmdk##*.} = vmdk ] || fail "Invalid input file '$vmdk'"
@@ -19,10 +34,10 @@ vmdk=$1; shift
 
 mf=${ovf%.*}.mf
 
-ovf_sha=$(sha1sum $ovf | awk '{print $1}')
-vmdk_sha=$(sha1sum $vmdk | awk '{print $1}')
+ovf_sha=$(${sha}sum $ovf | awk '{print $1}')
+vmdk_sha=$(${sha}sum $vmdk | awk '{print $1}')
 
 cat << EOF > $mf
-SHA1 ($(basename $ovf)) = $ovf_sha
-SHA1 ($(basename $vmdk)) = $vmdk_sha
+$SHA ($(basename $ovf)) = $ovf_sha
+$SHA ($(basename $vmdk)) = $vmdk_sha
 EOF
