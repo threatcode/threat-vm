a26d04ff9cd035ce2826f598b123ac6c4e9747fa
Ben Wilson <g0tmi1k@kali.org>
2023-03-06 13:46:15 +0000

    Add non-free-firmware components

--- scripts/finish-install.sh
+++ scripts/finish-install.sh
@@ -22,10 +22,10 @@ configure_apt_sources_list() {
 
     cat >/etc/apt/sources.list <<END
 # See https://www.kali.org/docs/general-use/kali-linux-sources-list-repositories/
-deb http://http.kali.org/kali kali-rolling main contrib non-free
+deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
 
 # Additional line for source packages
-# deb-src http://http.kali.org/kali kali-rolling main contrib non-free
+# deb-src http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware
 END
     apt-get update
 }
